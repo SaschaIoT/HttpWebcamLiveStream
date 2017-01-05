77957a44eb0fee52b3a28c4e108f2d715a30b0bd
Sascha <dummy@dummy.de>
2017-01-05 18:24:49 +0100

    Limit video frames per second (less network traffic)

--- HttpWebcamLiveStream/Web/JavaScript/Camera.js
+++ HttpWebcamLiveStream/Web/JavaScript/Camera.js
@@ -1,4 +1,14 @@
-﻿function GetVideoFrame() {
+﻿//Maximum frames of the video stream per second. The less frames the less network traffic.
+//(the current server side webcam stream delivers 30 frames, so I limit the frames to 30)
+var maxVideoFrames = 30;
+
+var xhttpRequestTimeout = 2000;
+var videoFrameTimeout = 1000.0 / maxVideoFrames;
+var lastVideoFrameTime = new Date();
+
+function GetVideoFrame() {
+
+    lastVideoFrameTime = new Date();
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://minwinpc/VideoFrame" + new Date().getTime().toString() + ".html", true);
@@ -16,18 +26,30 @@
 
     xhr.timeout = xhttpRequestTimeout;
     xhr.ontimeout = function () {
-        setTimeout(function () { GetVideoFrame(); }, 0);
+        GetVideoFrameAfterTimeout();
     }
 
     xhr.onerror = function () {
-        setTimeout(function () { GetVideoFrame(); }, 0);
+        GetVideoFrameAfterTimeout();
     }
 
     xhr.onload = function () {
-        setTimeout(function () { GetVideoFrame(); }, 0);
+        GetVideoFrameAfterTimeout();
     }
 
     xhr.send();
+}
+
+function GetVideoFrameAfterTimeout() {
+    var lastVideoFrameTimeToLast = new Date() - lastVideoFrameTime;
+
+    if (lastVideoFrameTimeToLast >= videoFrameTimeout) {
+        setTimeout(function () { GetVideoFrame(); }, 0);
+
+    } else {
+        var nextVideoFrameTimeout = videoFrameTimeout - lastVideoFrameTimeToLast;
+        setTimeout(function () { GetVideoFrame(); }, nextVideoFrameTimeout);
+    }
 }
 
 GetVideoFrame();
